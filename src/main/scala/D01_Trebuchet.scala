a58327e7ba6804b4f7c018e6553aac579b3cf604
Alessandro Marcantoni <alessandro.marcantoni.98@gmail.com>
2023-12-01 15:27:54 +0100

    chore: refactor day 1 (wrong)

--- src/main/scala/D01_Trebuchet.scala
+++ src/main/scala/D01_Trebuchet.scala
@@ -1,49 +1,32 @@
 import utils.Utils.readLines
 
 object D01_Trebuchet {
-  private type Index = Int
-
-  private val numbersMap: Map[String, Int] = Map(
-    "zero" -> 0,
-    "one" -> 1,
-    "two" -> 2,
-    "three" -> 3,
-    "four" -> 4,
-    "five" -> 5,
-    "six" -> 6,
-    "seven" -> 7,
-    "eight" -> 8,
-    "nine" -> 9
+  private val numbersMap: Map[Int, String] = Map(
+    1 -> "one",
+    2 -> "two",
+    3 -> "three",
+    4 -> "four",
+    5 -> "five",
+    6 -> "six",
+    7 -> "seven",
+    8 -> "eight",
+    9 -> "nine"
   )
 
-  private def indexesOf(
-      line: String,
-      toCheck: List[String],
-      getValue: String => Int
-  )(implicit numbersMap: Map[String, Int] = numbersMap): List[(Index, Int)] = toCheck
-    .flatMap(number =>
-      List(
-        (line.toCharArray.indexOfSlice(number), getValue(number)),
-        (line.toCharArray.lastIndexOfSlice(number), getValue(number))
-      )
-    )
-    .filter(_._1 >= 0)
+  private def replaceWords(line: String): String =
+    numbersMap.foldLeft(line) { case (acc, (key, value)) => acc.replaceAll(value, key.toString) }
+
+  private def solve(lines: List[String]): Int = lines
+    .map(_.toCharArray.filter(_.isDigit).toList)
+    .map(digits => (digits.head.toString, digits.last.toString))
+    .map(pair => (pair._1 + pair._2).toInt)
+    .sum
 
   def main(args: Array[String]): Unit = {
     val lines = readLines("01-trebuchet.txt")
-    val numbers = lines
-      .map(_.toCharArray.filter(_.isDigit).toList)
-      .map(digits => (digits.head.toString, digits.last.toString))
-      .map(pair => (pair._1 + pair._2).toInt)
-    val numbersWithLetters = lines
-      .map(line =>
-        indexesOf(line, numbersMap.keySet.toList, numbersMap(_)) ++
-          indexesOf(line, numbersMap.values.toList.map(_.toString), _.toInt)
-      )
-      .map(line => (line.minBy(_._1)._2.toString, line.maxBy(_._1)._2.toString))
-      .map(pair => (pair._1 + pair._2).toInt)
+    val linesWithWords = lines.map(replaceWords)
 
-    println(numbers.sum)
-    print(numbersWithLetters.sum)
+    println(solve(lines))
+    println(solve(linesWithWords))
   }
 }
